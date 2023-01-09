31d1ead63f18034901bf77f03ded8c89109f933d
Wuxinyue <2467637323@qq.com>
2023-01-09 12:52:26 +0800

    调整项目结构

--- consumer/src/test/java/com/wxy/rpc/ConsumerApplicationTest.java
+++ consumer/src/test/java/com/wxy/rpc/ConsumerApplicationTest.java
@@ -8,31 +8,27 @@ import junit.framework.TestSuite;
  * Unit test for simple App.
  */
 public class ConsumerApplicationTest
-    extends TestCase
-{
+        extends TestCase {
     /**
      * Create the test case
      *
      * @param testName name of the test case
      */
-    public ConsumerApplicationTest(String testName )
-    {
-        super( testName );
+    public ConsumerApplicationTest(String testName) {
+        super(testName);
     }
 
     /**
      * @return the suite of tests being tested
      */
-    public static Test suite()
-    {
-        return new TestSuite( ConsumerApplicationTest.class );
+    public static Test suite() {
+        return new TestSuite(ConsumerApplicationTest.class);
     }
 
     /**
      * Rigourous Test :-)
      */
-    public void testApp()
-    {
-        assertTrue( true );
+    public void testApp() {
+        assertTrue(true);
     }
 }
